da3c603fa6bd212d3902c46cbae21c673a07ae79
COMRADE040 <rafiulislam040@gmail.com>
2026-02-24 04:52:16 +0600

    private route init

--- src/proxy.js
+++ src/proxy.js
@@ -2,10 +2,28 @@ import { getToken } from 'next-auth/jwt'
 import { NextResponse } from 'next/server'
 // import type { NextRequest } from 'next/server'
 
+
+
+const privateRoutes = [
+    "/private",
+    "/dashbaord",
+    "/secret"
+]
 export async function proxy(req) {
     const token = await getToken({req});
-    console.log(token)
+    const reqPath = req.nextUrl.pathname;
+    const isAuthenticated = Boolean(token)
+    const isUser = token?.role === "user";
+    const isAdmin = token?.role === "admin"
+    const isPrivate = privateRoutes.some(route => reqPath.startsWith(route))
+    console.log({isAuthenticated, isUser, reqPath, isPrivate})
 
+
+    if(!isAuthenticated && isPrivate){
+        const loginUrl = new URL("/api/auth/signin", req.url)
+        loginUrl.searchParams.set("callbackUrl", reqPath)
+        return NextResponse.redirect(loginUrl)
+    }
 
     // return NextResponse.redirect(new URL('/home', request.url))
     return NextResponse.next()
@@ -15,5 +33,5 @@ export async function proxy(req) {
 // export default function proxy(request: NextRequest) { ... }
  
 export const config = {
-  matcher: '/private/:path*',
+  matcher: ['/private/:path*', '/dashboard/:path*', '/secret/:path*'],
 }
