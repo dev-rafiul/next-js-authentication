68d7d126c2639ab268d4851ca47f2385062e84cf
COMRADE040 <rafiulislam040@gmail.com>
2026-02-23 22:18:44 +0600

    authentaction json

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,54 @@
+import NextAuth from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials"
+
+
+
+
+const userList = [
+  {name: "hablu", password: "1234"},
+  {name: "dablu", password: "5678"},
+  {name: "bablu", password: "9101"},
+]
+
+
+export const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    CredentialsProvider({
+    // The name to display on the sign in form (e.g. 'Sign in with...')
+    name: 'Email and Password',
+  
+    //from inputs
+
+    credentials: {
+      username: { label: "Username", type: "text", placeholder: "jsmith" },
+      password: { label: "Password", type: "password" },
+      secretCode: {label: "Secret Code", type: "number", placeholder: "Enter Code"}
+    },
+    async authorize(credentials, req) {
+
+
+      const {username, password, secretCode} = credentials;
+
+      const user = userList.find(u => u.name == username)
+
+      if(!user) return null
+
+      const isPasswordOk = user.password == password;
+
+      if(isPasswordOk){
+        return user
+      }
+      
+      
+      // Return null if user data could not be retrieved
+      return null
+    }
+  })
+    // ...add more providers here
+  ],
+}
+
+const handler = NextAuth(authOptions)
+
+export {handler as GET, handler as POST}
---
+++ src/components/LoginButton.jsx
@@ -0,0 +1,9 @@
+"use client"
+import React from 'react';
+import {signIn} from "next-auth/react"
+
+const LoginButton = () => {
+    return <button onClick={() => signIn()} className='btn'>Login Now</button>
+};
+
+export default LoginButton;
